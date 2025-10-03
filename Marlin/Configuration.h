814fdf1fcd7b1a60ec659ea7ac618a9d52e20d51
Reimnop <rayngu2005@gmail.com>
2025-10-03 23:33:54 +0700

    🔧 Redefine build volume

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1903,14 +1903,14 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 245
+#define X_BED_SIZE 235
 #define Y_BED_SIZE 235
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
+#define X_MAX_POS (X_BED_SIZE + 10)
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 250
 //#define I_MIN_POS 0
